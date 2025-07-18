17542c7511eddf6bd73a1b776a1cdc3eb0869033
izzetp <135380394+izzetp@users.noreply.github.com>
2025-07-18 12:42:28 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -9,6 +9,10 @@ redis_host = os.getenv("REDIS_HOST", "redis")
 redis_port = int(os.getenv("REDIS_PORT", "6379"))
 r = redis.Redis(host=redis_host, port=redis_port, decode_responses=True)
 
+@app.get("/")
+def read_root():
+    return {"message": "FastAPI with Redis is working!"}
+
 @app.get("/weather/{city}")
 def get_weather(city: str):
     if r.exists(city):
