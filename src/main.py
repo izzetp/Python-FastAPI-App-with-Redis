0d7945c1c18d00d03d6db4eaf828bdf21613b11e
izzetp <135380394+izzetp@users.noreply.github.com>
2025-07-18 12:04:23 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -6,3 +6,14 @@ import os
 app = FASTAPI()
 
 redis_host = os.getenv("REDIS_HOST", "redis")
+redis_port = int(os.getenv("REDIS_PORT", "6379"))
+r = redis.Redis(host=redis_host, port=redis_port, decode_responses=True)
+
+@app.get("/weather/{city}")
+def get_weather(city: str):
+    if r.exists(city):
+        return {"source" : "cache", "data" : r.get(city)}
+
+    response = requests.get(f"https://wttr.in/{city}?format=3")
+    r.setex(city, 60, response.text)
+    return {"source" : "api", "data" : response.text}
